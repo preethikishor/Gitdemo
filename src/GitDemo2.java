69ad7ce829056ed42047422fdfb50d2539ea10da
preethikishor <preethidj@gmail.com>
2019-03-08 15:03:26 +0530

    SSSS

--- src/GitDemo2.java
+++ src/GitDemo2.java
@@ -3,7 +3,7 @@ public class GitDemo2 {
 
 	public static void main(String[] args) {
 		System.out.println("123  moved to california ");
-
+		System.out.println("1299 moved to california ");
 	}
 
 }
